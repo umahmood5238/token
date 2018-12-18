7e78a836763ae61da060da0b04283786847e6dc5
BT_1N3_29@bktech.local <BT_1N3_29@bktech.local>
2018-12-18 08:40:33 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	TokenPass testGame = new TokenPass(10);
+
+	int [] testBoard = testGame.getBoard();
+	System.out.println("Constructed a testBoard");
+	for (int i = 0; i < testBoard.length; ) {
+	    System.out.println(testBoard[i] + " ");
+    }
+    System.out.println();
+
+	testGame.distributeCurrentPlayerTokens();
+	System.out.println("After distributing tokens from player " + testGame.getCurrentPlayer() + ": ");
+	for (int i = 0; i < testBoard.length; i++) {
+	    System.out.println(testBoard[i] + " ");
+    }
+    }
+}
